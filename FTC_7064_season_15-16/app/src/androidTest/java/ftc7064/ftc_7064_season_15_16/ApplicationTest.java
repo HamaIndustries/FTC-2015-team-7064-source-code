82020e29c7288b97038ac4a09051d08b216fc85d
Elijah Mitchell <eliatlarge@gmail.com>
2015-10-05 16:22:14 -0500

    Add fresh project base

---
+++ FTC_7064_season_15-16/app/src/androidTest/java/ftc7064/ftc_7064_season_15_16/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ftc7064.ftc_7064_season_15_16;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
